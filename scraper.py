23d74fcd12dea595a26d9fd01bcb9b5c142beb92
PreetiSharma15 <pwhitehat6@gmail.com>
2022-04-13 18:37:31 +0530

    Updated with XPATH method and Pandas

--- scraper.py
+++ scraper.py
@@ -1,36 +1,59 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
-import csv
+import pandas as pd
+
+# NASA Exoplanet URL
 START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
-browser = webdriver.Edge("./msedgedriver.exe")
+
+# Webdriver
+browser = webdriver.Chrome("D:/Setup/chromedriver_win32/chromedriver.exe")
 browser.get(START_URL)
+
 time.sleep(10)
+
+planets_data = []
+
+# Define Exoplanet Data Scrapping Method
 def scrape():
-    headers = ["name", "light_years_from_earth", 
-                "planet_mass", "stellar_magnitude", "discovery_date"]
-    planet_data = []
-    for i in range(0,490):
+
+    for i in range(0,10):
+        print(f'Scrapping page {i+1} ...' )
+        
+        # BeautifulSoup Object     
         soup = BeautifulSoup(browser.page_source, "html.parser")
+
+        # Loop to find element using XPATH
         for ul_tag in soup.find_all("ul", attrs={"class", "exoplanet"}):
+
             li_tags = ul_tag.find_all("li")
+           
             temp_list = []
+
             for index, li_tag in enumerate(li_tags):
-                if index == 0:
+
+                if index == 0:                   
                     temp_list.append(li_tag.find_all("a")[0].contents[0])
                 else:
                     try:
                         temp_list.append(li_tag.contents[0])
                     except:
                         temp_list.append("")
-            planet_data.append(temp_list)
-        browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
-    print(planet_data[1])
 
-    with open("scraper_2.csv", "w") as f:
-       csvwriter = csv.writer(f)
-        csvwriter.writerow(headers)
-        csvwriter.writerows(planet_data)
+            planets_data.append(temp_list)
+
+        # Find all elements on the page and click to move to the next page
+        browser.find_element(by=By.XPATH, value='//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+
+# Calling Method    
 scrape()
 
+# Define Header
+headers = ["name", "light_years_from_earth", "planet_mass", "stellar_magnitude", "discovery_date"]
 
+# Define pandas DataFrame   
+planet_df_1 = pd.DataFrame(planets_data, columns=headers)
+
+# Convert to CSV
+planet_df_1.to_csv('scrapped_data.csv',index=True, index_label="id")
